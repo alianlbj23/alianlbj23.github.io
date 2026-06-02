4e04279f762a4444873b27f87f5704210ea3c56c
alianlbj23 <alianlbj23@gmail.com>
2026-06-02 14:31:32 +0900

    Disable the open animation

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to false if you don't want Splash screen.
+  isSplash: false, // Change this to false if you don't want Splash screen.
 };
 
 //SEO Related settings
@@ -13,7 +13,7 @@ const seo = {
   og: {
     title: "Devin Tseng Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "https://alianlbj23.github.io/home",
   },
 };
 
